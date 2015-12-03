f669cbae23858ea05dfa9c7ea49e80fee865ba7f
pierreFauche <pierre.fauche@gmail.com>
2015-12-03 20:53:36 +0100

    azr

--- ProjectSpark/src/part1DF.java
+++ ProjectSpark/src/part1DF.java
@@ -1,57 +1,33 @@
-import au.com.bytecode.opencsv.CSVParser
-import org.apache.spark.rdd.RDD
-
-val crimeFile = "crimes.csv"
-val crimeData = sc.textFile(crimeFile).cache()
+val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+import sqlContext.implicits._
 
 
+case class Cast(cdatetime:String,address:String,district:Int,beat:String,grid:Int,crimedescr:String,ucr_ncic_code:Int,latitude:Float,longitude:Float)
+
+val file = sc.textFile("crimes.csv").
+mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else
+iter }
+
+val casts = file.map(line => {
+val l = line.split(",")
+Cast(l(0).split(" ")(0),l(1), l(2).toInt, l(3),l(4).toInt,l(5),l(6).toInt,l(7).toFloat,l(8).toFloat)
+})
+
+val castsDF = casts.toDF
+
+castsDF.show
 
 
+ //1
 
-crimeData.mapPartitions(lines => {
-         val parser = new CSVParser(',')
-         lines.map(line => {
-           parser.parseLine(line).mkString(",")
-         })
-       }).take(5).foreach(println)
+	 //le bon
+	 castsDF.groupBy('crimedescr).count().orderBy(desc("count")).show(1)
+	 castsDF.groupBy('ucr_ncic_code).count().orderBy(desc("count")).show(1)
 
-def dropHeader(data: RDD[String]): RDD[String] = {
-         data.mapPartitionsWithIndex((idx, lines) => {
-           if (idx == 0) {
-             lines.drop(1)
-           }
-           lines
-         })
-       }
+ //2
 
-val withoutHeader: RDD[String] = dropHeader(crimeData)
-
-withoutHeader.mapPartitions(lines => {
-         val parser = new CSVParser(',')
-         lines.map(line => {
-           parser.parseLine(line).mkString(",")
-         })
-       }).take(5).foreach(println)
-
-
-/////////////////////////////////////Ma reponse question 1
-    crimeData.mapPartitions(lines => {
-      val parser=new CSVParser(',');
-      lines.map(line => {
-        val columns = parser.parseLine(line)
-        Array(columns(5)).mkString(",")
-      })
-    }).countByValue().maxBy(_._2)._1;
-
-//////////////////////////////////////// Ma reponse question 2
-
-
-  
-    crimeData.mapPartitions(lines => {
-      val parser=new CSVParser(',');
-      lines.map(line => {
-        val columns = parser.parseLine(line)
-        Array(columns(0).split(" ")(0)).mkString(",")         
-
-          })
-    }).countByValue().toList.sortBy(-_._2).take(3).foreach(println)
+ 	 castsDF.groupBy('cdatetime).count().orderBy('count.desc).show(3)
+ //3
+	    
+	 val nbCrimes = castsDF.groupBy('crimedescr).count().orderBy(desc("count"))
+	 nbCrimes.select(nbCrimes("crimedescr"), nbCrimes("count")/(31)).show()
